508d3bfe515b38b6cf2e91acac627241f8e0e457
Hereward Cooper <coops@fawk.eu>
2023-09-21 15:29:47 -0700

    support forcing state and cleaning state on disconnect

--- public/index.js
+++ public/index.js
@@ -1,11 +1,40 @@
 window.onload = function () {
+
+  const sio = io();
+  updateStatus('DISCONNECTED');
+  sio.on('connect', () => {
+    console.log('connected as', sio.id);
+    sio.emit('force_update');
     updateStatus('', 'green');
+  });
+
+  sio.on('disconnect', () => {
+    console.log('disconnected');
+    forceState('hidden');
     updateStatus('DISCONNECTED', 'red');
+  });
 
   // Listen for all incoming events
   sio.onAny((event, ...args) => {
     console.log(sio.id, 'Received event:', event, args[0]);
   });
+
+  sio.on('update_state', (data) => {
+    e = document.getElementById(data.element_id).style.visibility = data.visibility;
+  });
+
+
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has('forceon')) {
+    forceState('visible');
+  }
+
+  function forceState(state) {
+    const spans = document.querySelectorAll(".wrap span");
+    spans.forEach(span => {
+      span.style.visibility = state;
+    });
+  }
 
   function updateStatus(msg, clr) {
     div = document.getElementById('status');
