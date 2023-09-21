664b88106108bbb47a60dd9429669ca0740f0528
Hereward Cooper <coops@fawk.eu>
2023-09-21 15:28:41 -0700

    connect/disconnect message

--- public/index.js
+++ public/index.js
@@ -1,16 +1,13 @@
-const sio = io();
-
-sio.on('connect', () => {
-  console.log('connected');
-  sio.emit('force_update');
-});
+    updateStatus('', 'green');
+    updateStatus('DISCONNECTED', 'red');
 
   // Listen for all incoming events
   sio.onAny((event, ...args) => {
     console.log(sio.id, 'Received event:', event, args[0]);
   });
 
-sio.on('update_state', (data) => {
-  console.log("Got update:", data);
-  e = document.getElementById(data.element_id).style.visibility = data.visibility;
-});
+  function updateStatus(msg, clr) {
+    div = document.getElementById('status');
+    div.innerHTML=msg;
+    div.style.color=clr;
+  }
